82c5c7b6b7ba8024775e916b0336d517b51293a1
Julio Makdisse Saito <saitodisse@gmail.com>
2016-12-16 04:11:22 -0200

    feat(Input): add input[range]

--- src/components/SumRotation/index.js
+++ src/components/SumRotation/index.js
@@ -7,7 +7,10 @@ export default connect({},
   function SumRotation () {
     return (
       <div className='page-container'>
-        <h2>SumRotation</h2>
+        <h2>
+          01 - Sum rotation
+        </h2>
+        <input id='test' type='range' min='-2' max='2' step='0.01' value='0.05' />
         <Canvas zoomLevel={1} />
       </div>
     )
