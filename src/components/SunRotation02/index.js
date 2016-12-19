96c535cfe668538ff10906be705378c70e8b0051
Julio Makdisse Saito <saitodisse@gmail.com>
2016-12-19 16:41:11 -0200

    feat(Sun2): Removed variable state from cerebral state

--- src/components/SunRotation02/index.js
+++ src/components/SunRotation02/index.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'cerebral/react'
 import Canvas from '../Canvas'
 import './styles.css'
 
 export default connect({
-    rotationSpeed: 'sunRotation02Module.rotationSpeed',
-    reverseAcceleration: 'sunRotation02Module.reverseAcceleration',
-    MAX_SPEED: 'sunRotation02Module.MAX_SPEED'
-  }, {
-    rotationSpeedChanged: 'sunRotation02Module.rotationSpeedChanged',
-    reverseAccelerationChanged: 'sunRotation02Module.reverseAccelerationChanged'
-  },
-  function SunRotation (props) {
-    const _onStart = ({ctx, PIXI, canvasSize}) => {
+  initialValues: 'sunRotation02Module.initialValues'
+},
+  class SunRotation extends Component {
+    constructor (props) {
+      super(props)
+      this.state = Object.assign({}, this.props.initialValues)
+      console.log(this.state)
+    }
+
+    _onSunMouseDown (ctx, mouseData) {
+      const START = 255
+      const SUN_WIDTH = ctx.sun.width
+      const clickedAtX = mouseData.data.global.x
+      const MIDDLE = START + (SUN_WIDTH / 2)
+      const OFFSET_X = MIDDLE - clickedAtX
+      this.setState({
+        rotationSpeed: this.state.MAX_SPEED * (OFFSET_X / (SUN_WIDTH / 2))
+      })
+    }
+
+    _onStart ({ctx, PIXI, canvasSize}) {
       // create a texture from an image path
       const texture = PIXI.Texture.fromImage('sun.png')
 
@@ -29,28 +41,20 @@ export default connect({
 
       ctx.stage.addChild(ctx.sun)
 
-      function onDown (mouseData) {
-        const sunWidth = ctx.sun.width
-        const maxWidthFromCenter = sunWidth / 2
-        const localCoordsPosition = mouseData.data.getLocalPosition(ctx.sun)
-        const ratio = localCoordsPosition.x / maxWidthFromCenter
-        props.rotationSpeedChanged({speed: props.MAX_SPEED * ratio})
-      }
-
       // mouse event
       ctx.sun.interactive = true
-      ctx.sun.on('mousedown', onDown)
-      ctx.sun.on('touchstart', onDown)
+      ctx.sun.on('mousedown', (mouseData) => this._onSunMouseDown(ctx, mouseData))
+      ctx.sun.on('touchstart', (mouseData) => this._onSunMouseDown(ctx, mouseData))
     }
 
-    const _onAnimate = (ctx) => {
+    _onAnimate (ctx) {
       let newSpeed = 0
-      let absRotationSpeed = Math.abs(props.rotationSpeed * 1000)
+      let absRotationSpeed = Math.abs(this.state.rotationSpeed * 1000)
 
       // change velocity
-      if (props.rotationSpeed !== 0 &&
+      if (this.state.rotationSpeed !== 0 &&
         absRotationSpeed > 0) {
-        newSpeed = props.rotationSpeed - (props.rotationSpeed * props.reverseAcceleration)
+        newSpeed = this.state.rotationSpeed - (this.state.rotationSpeed * this.state.reverseAcceleration)
       }
 
       // too slow, must stop
@@ -58,67 +62,77 @@ export default connect({
         newSpeed = 0
       }
 
-      if (props.rotationSpeed !== newSpeed) {
+      if (this.state.rotationSpeed !== newSpeed) {
         // update state
-        props.rotationSpeedChanged({speed: newSpeed})
+        this.setState({
+          rotationSpeed: newSpeed
+        })
       }
 
       ctx.sun.rotation += newSpeed
     }
 
-
-    const number = -0.40
-    return (
-      <div className='page-container'>
-        <h2>
-          02 - Sun rotation that stops
-        </h2>
-        <div className='bodyContent'>
-          <div className='controlsContainer'>
-            <div className='inputContainer'>
-              <label htmlFor='test'>
-                Initial Speed:
-              </label>
-              <span>
-                {Math.floor(props.rotationSpeed * 1000)}
-              </span>
-              <input
-                id='test'
-                type='range'
-                min={-props.MAX_SPEED}
-                max={props.MAX_SPEED}
-                step='0.01'
-                value={props.rotationSpeed}
-                onChange={(e) => props.rotationSpeedChanged({speed: Number(e.target.value)})}
+    render (props) {
+      return (
+        <div className='page-container'>
+          <h2>
+            02 - Sun rotation that stops
+          </h2>
+          <div className='bodyContent'>
+            <div className='controlsContainer'>
+              <div className='inputContainer'>
+                <label htmlFor='test'>
+                  Initial Speed:
+                </label>
+                <span>
+                  {Math.floor(this.state.rotationSpeed * 1000)}
+                </span>
+                <input
+                  id='test'
+                  type='range'
+                  min={-this.state.MAX_SPEED}
+                  max={this.state.MAX_SPEED}
+                  step='0.01'
+                  value={this.state.rotationSpeed}
+                  onChange={(e) => {
+                    this.setState({
+                      rotationSpeed: Number(e.target.value)
+                    })
+                  }}
+                />
+              </div>
+              <div className='inputContainer'>
+                <label htmlFor='acceleration'>
+                  Reverse acceleration:
+                </label>
+                <span>
+                  {this.state.reverseAcceleration}
+                </span>
+                <input
+                  id='acceleration'
+                  type='range'
+                  min='0.00'
+                  max='0.10'
+                  step='0.01'
+                  value={this.state.reverseAcceleration}
+                  onChange={(e) => {
+                    this.setState({
+                      reverseAcceleration: Number(e.target.value)
+                    })
+                  }}
+                />
+              </div>
+            </div>
+            <div className='canvasContainer'>
+              <Canvas
+                zoomLevel={1}
+                onStart={(ctx) => this._onStart(ctx)}
+                onAnimate={(ctx) => this._onAnimate(ctx)}
               />
             </div>
-            <div className='inputContainer'>
-              <label htmlFor='acceleration'>
-                Reverse acceleration:
-              </label>
-              <span>
-                {props.reverseAcceleration}
-              </span>
-              <input
-                id='acceleration'
-                type='range'
-                min='0.00'
-                max='0.10'
-                step='0.01'
-                value={props.reverseAcceleration}
-                onChange={(e) => props.reverseAccelerationChanged({acceleration: Number(e.target.value)})}
-              />
-            </div>
-          </div>
-          <div className='canvasContainer'>
-            <Canvas
-              zoomLevel={1}
-              onStart={_onStart}
-              onAnimate={_onAnimate}
-            />
           </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
 )
--- src/modules/sunRotation02Module.js
+++ src/modules/sunRotation02Module.js
@@ -1,21 +1,17 @@
-import { set, state, input } from 'cerebral/operators'
+import { set, state } from 'cerebral/operators'
 
 export default {
   state: {
-    rotationSpeed: -0.20,
-    reverseAcceleration: 0.05,
-    MAX_SPEED: 0.40,
-    playing: false
+    initialValues: {
+      rotationSpeed: 1,
+      reverseAcceleration: 0.05,
+      MAX_SPEED: 0.40,
+      playing: false
+    }
   },
   signals: {
     routed: [
       set(state`currentPage`, 'sunRotation02')
-    ],
-    rotationSpeedChanged: [
-      set(state`sunRotation02Module.rotationSpeed`, input`speed`)
-    ],
-    reverseAccelerationChanged: [
-      set(state`sunRotation02Module.reverseAcceleration`, input`acceleration`)
     ]
   }
 }
