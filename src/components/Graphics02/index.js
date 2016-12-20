7fde137c744c1705dff35ddc41b4456aece642d5
Julio Makdisse Saito <saitodisse@gmail.com>
2016-12-20 00:26:15 -0200

    feat(Graphics02): add a graphic for each square, clickable

--- src/components/Graphics02/index.js
+++ src/components/Graphics02/index.js
@@ -16,48 +16,41 @@ export default connect({
     }
 
     _drawGrid (ctx) {
-      // create the root of the scene graph
-      ctx.stage = new PIXI.Container()
       const squareSize = ctx.canvasSize.width / this.state.gridSize
       this._renderSquaresAllCanvas(ctx, squareSize)
-
-      // // mouse event
-      // ctx.stage.interactive = true
-      // ctx.stage.on('mousedown', (mouseData) => this._onSunMouseDown(ctx, mouseData))
-      // ctx.stage.on('touchstart', (mouseData) => this._onSunMouseDown(ctx, mouseData))
-    }
-
-    _onSunMouseDown (ctx, mouseData) {
-      console.log(`--mouseData.data.target.graphicsData--`); console.log(mouseData.data.target.graphicsData.map(g => g.fillColor)) // DEBUG
     }
 
     _renderSquaresAllCanvas (ctx, size) {
       const maxHeight = ctx.canvasSize.height
       for (let i = 0; size * i < maxHeight; i++) {
-        this._fullRow(ctx, size, size * i, i + 1)
+        this._fullRow(ctx, size, size * i, i)
       }
     }
 
-    _fullRow (ctx, width, startOnHeight, index) {
+    _fullRow (ctx, width, startOnHeight, rowIndex) {
       const maxLength = ctx.canvasSize.width
       let startPoint = [0, startOnHeight]
       let endPoint = [width, width + startOnHeight]
       let columnIndex = 0
       while (startPoint[0] < maxLength) {
-        this._drawSquare(ctx, startPoint, endPoint, columnIndex)
+        this._drawSquare(ctx, startPoint, endPoint, rowIndex, columnIndex)
         startPoint[0] += width
         endPoint[0] += width
         columnIndex++
       }
     }
 
-    _drawSquare (ctx, initial, final, index) {
+    _drawSquare (ctx, initial, final, rowIndex, colIndex) {
       const square = new PIXI.Graphics()
       // set a fill and line style
       square.lineStyle(1, 0x6666ee, 1)
       // draw a shape
       square.moveTo(...initial)
-      square.beginFill(this.state.colors[index % this.state.colors.length])
+
+      // get color colIndex
+      const colorIndex = this.state.allItemsColors[rowIndex][colIndex]
+      const currentFillColor = this.state.colors[colorIndex % this.state.colors.length]
+      square.beginFill(currentFillColor)
       square.lineTo(final[0], initial[1])
       square.lineTo(...final)
       square.lineTo(initial[0], final[1])
@@ -66,15 +59,23 @@ export default connect({
 
       // mouse event
       square.interactive = true
-      square.on('mousedown', (mouseData) => this._onSunMouseDown(ctx, mouseData))
-      square.on('touchstart', (mouseData) => this._onSunMouseDown(ctx, mouseData))
+      square.on('mousedown', (mouseData) => this._onSquarePressed(ctx, rowIndex, colIndex))
+      square.on('touchstart', (mouseData) => this._onSquarePressed(ctx, rowIndex, colIndex))
 
       ctx.stage.addChild(square)
     }
 
     _onStart ({ctx}) {
-      this._drawGrid(ctx)
-      window.ctx = ctx
+      ctx.stage = new PIXI.Container()
+
+      // new Array, one color for each item
+      const allItemsColors = new Array(this.state.gridSize)
+        .fill(null)
+        .map(a => new Array(this.state.gridSize).fill(0))
+
+      this.setState({allItemsColors}, () => {
+        this._drawGrid(ctx)
+      })
     }
 
     _onAnimate (ctx) {
@@ -85,6 +86,14 @@ export default connect({
           mustRedrawGrid: false
         })
       }
+    }
+
+    _onSquarePressed (ctx, rowIndex, colIndex) {
+      const allItemsColors = this.state.allItemsColors
+      allItemsColors[rowIndex][colIndex] += 1
+      this.setState({allItemsColors})
+      // ctx.stage.destroy()
+      this._drawGrid(ctx)
     }
 
     render () {
